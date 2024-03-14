f47e43b76b660d3d8e997cc4eb1a245e90934651
groww-tech <siddharthcwork01@gmail.com>
2024-03-14 20:41:27 +0530

    update contract info

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.23;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract DEFIStaking {
+contract Staking {
     using SafeERC20 for IERC20;
 
     uint256 constant DAY_IN_SECONDS = 86400;
